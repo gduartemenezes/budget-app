92bfe105d2685e0756835212186efb8c99fb64e4
GustavoD <gduartemenezes@gmail.com>
2020-03-20 18:59:26 -0300

    Updated controller to handle budget feature

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,32 @@
 /*jshint esversion: 8 */
 import Input from './models/Input';
+import Budget from './models/Budget';
 import * as inputView from './views/inputView';
 import {elements} from './views/base';
 
 
-const state = {};
 
-const controlBudget = () => {
+const state = {};
+const controlInput = () => {
     const data = inputView.getInput();
     if(data){
         state.input = new Input(data);
     }
-    console.log(state.input);
+
 };
 
-elements.addBtn.addEventListener('click', e => controlBudget());
+const controlBudget = () => {
+    if(!state.budget) state.budget = new Budget();
+    const el = state.input.data;
+
+    state.budget.addItem(el.type, el.desc, el.value);
+    console.log(state.budget);
+   
+};
+
+
+elements.addBtn.addEventListener('click', () => {
+    controlInput();
+    controlBudget();
+    
+});
