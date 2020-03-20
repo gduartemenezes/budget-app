e951af079685b7f3b947e031e1f61a9e9bd4166c
GustavoD <gduartemenezes@gmail.com>
2020-03-20 17:09:13 -0300

    Base module, exports classes names to general use

---
+++ src/js/views/base.js
@@ -0,0 +1,8 @@
+/* jshint esversion: 8*/
+export const elements = {
+    inputType: document.querySelector('.add__type'),
+    inputDesc: document.querySelector('.add__description'),
+    inputValue: document.querySelector('.add__value'),
+    addBtn: document.querySelector('.add__btn')
+};
+
