db7af49f7b7d3d762b6b1ee5b82ae9ccedbcb4c5
GustavoD <gduartemenezes@gmail.com>
2020-03-21 16:31:28 -0300

    Updated to generate an id when the item is also generated

--- src/js/models/Input.js
+++ src/js/models/Input.js
@@ -1,7 +1,8 @@
 /* jshint esversion:8 */
-
+import uniqid from 'uniqid';
 export default class Input{
     constructor(data){
+        this.id = uniqid();
         this.type = data.type;
         this.desc = data.desc;
         this.value = data.value;        
