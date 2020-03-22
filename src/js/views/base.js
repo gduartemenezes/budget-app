a9612a05bb34dd09cbda5f75ce95f3e3d1048d91
GustavoD <gduartemenezes@gmail.com>
2020-03-21 22:07:29 -0300

    Added some new items to elements obj

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -3,6 +3,13 @@ export const elements = {
     inputType: document.querySelector('.add__type'),
     inputDesc: document.querySelector('.add__description'),
     inputValue: document.querySelector('.add__value'),
-    addBtn: document.querySelector('.add__btn')
+    addBtn: document.querySelector('.add__btn'),
+    listContainer: document.querySelector('.container'),
+    incList: document.querySelector('.income__list'),
+    expList: document.querySelector('.expenses__list'),
+    budgetValue: document.querySelector('.budget__value'),
+    incTotal: document.querySelector('.budget__income--value'),
+    expTotal: document.querySelector('.budget__expenses--value'),
+    percentageTotal: document.querySelector('.budget__expenses--percentage')
 };
 
