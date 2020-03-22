3b5c4995ce8b6e44de9a652a7b920585a9538627
GustavoD <gduartemenezes@gmail.com>
2020-03-21 22:08:20 -0300

    Added Init function to render initial UI, updated controllers

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,24 @@
 import Input from './models/Input';
 import Budget from './models/Budget';
 import * as inputView from './views/inputView';
+import * as budgetView from './views/budgetView';
 import {elements} from './views/base';
 
 
+
+const initial = () => {
+    elements.budgetValue.textContent = 0;
+    elements.incTotal.textContent =  0;
+    elements.expTotal.textContent = 0;
+    elements.percentageTotal.textContent = '-- %';
+    elements.inputType.value = '';
+    elements.inputDesc.value = '';
+    elements.inputValue.value = '';
+};
 const state = {};
+initial();
+
+
 const controlInput = () => {
     // get user's Input
     const data = inputView.getInput();
@@ -13,20 +27,51 @@ const controlInput = () => {
         state.input = new Input(data);
     }
     console.log(state.input);
+    //render input to UI
+    inputView.renderItem(state.input);
 };
 
 const controlBudget = () => {
+    //creates a new budget
     if(!state.budget) state.budget = new Budget();
     const el = state.input;
-    state.budget.addItem(el.type, el.desc, el.value);
+    // add new item 
+    state.budget.addItem(el.id, el.type, el.desc, el.value);
+    // calculate the current budget
     state.budget.calcBudget();
     console.log('my budget:'  + state.budget.budget);
-   
+    // calc each exp percentage 
+    budgetView.renderBudget(state.budget);
+
+    //calc all exp percentage
 };
 
 
+/////////////////// UI HANDLERS
+// ADD ITEM HANDLER
 elements.addBtn.addEventListener('click', () => {
+    
     controlInput();
     controlBudget();
+    
+});
+
+
+// DELETE ITEM HANDLER
+elements.listContainer.addEventListener('click', e =>{
+    // gets item id  
+    let itemID;
+      if(e.target.closest('.item')){
+            itemID = e.target.closest('.item').id;
+      }
+      
+    // delete item UI and Data Structure
+    inputView.deleteItem(itemID);
+    state.budget.deleteItem(itemID);
+
+    // Update the Budget UI and Data Structure
+    state.budget.calcBudget();
+    
+
     
 });
